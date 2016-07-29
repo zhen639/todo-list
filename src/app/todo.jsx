4cc91eaf56d2f5f57dcfcf9df94d0a03ca8a007a
Caizhen Liu <czliu@CNczliu.local>
2016-07-29 21:56:19 +0800

    add todo task by hard code

--- src/app/todo.jsx
+++ src/app/todo.jsx
@@ -58,3 +58,39 @@ const todoApp = combineReducers({
 
 const store = createStore(todoApp)
 
+let nextTodoId = 0
+
+class TodoApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <button onClick={() => {
+          store.dispatch({
+            type: 'ADD_TODO',
+            text: 'Test',
+            id: nextTodoId++
+          })
+        }}>
+          Add Todo
+        </button>
+        <ul>
+          {this.props.todos.map(todo =>
+            <li key={todo.id}>
+              {todo.text}
+            </li>
+          )}
+        </ul>
+      </div>
+    )
+  }
+}
+
+const render = () => {
+  ReactDOM.render(
+    <TodoApp todos={store.getState().todos} />,
+    document.getElementById('root')
+  )
+}
+
+store.subscribe(render)
+render()
